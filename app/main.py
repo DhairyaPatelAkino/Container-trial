5ecfcf25398f623e33e5821bf57c04e1d807cf7d
dhairya patel <dhairya.patel@akinolabs.com>
2026-03-20 10:30:21 +0530

    health

--- app/main.py
+++ app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI in Docker!"}
+
+@app.get("/health")
+def health():
+    return {"message": "First class health"}
